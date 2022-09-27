943e9545433910aededf8776caea90aa1138818b
Ivan Ustyantsev <vangold915@gmail.com>
2022-09-27 20:00:20 +0700

    Completed task 23

---
+++ task23/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.Clear();
+Console.WriteLine("Вы находитесь в программе, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. Для продолжения нажмите Enter...");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= number; i++)
+{   
+    if (i == number) {
+        int mult = i * i * i;
+        Console.Write($"{mult}.");
+    } else {
+        int mult = i * i * i;
+        Console.Write($"{mult}, ");
+    }
+}
+
